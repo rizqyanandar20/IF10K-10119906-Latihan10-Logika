5de05783dfa5aba763fc440867d6821e4d89e119
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-10-21 07:00:29 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan10/logika/IF10K10119906Latihan10Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan10.logika;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama      : Rizqy Ananda Rusmana
+ * NIM       : 10119906
+ * Kelas     : IF-10K
+ * Deskripsi : Logika Dasar.
+ */
+public class IF10K10119906Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int umur  = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : "+ dewasa);
+        
+    }
+    
+}
